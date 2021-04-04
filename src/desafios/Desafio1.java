50f0fef79954b71d48402a34b94270cf158f2915
Marcelo Alves <marceloalves1295@gmail.com>
2021-04-04 15:45:42 -0300

    Primeiro desafio concluido

--- src/desafios/Desafio1.java
+++ src/desafios/Desafio1.java
@@ -1,9 +1,24 @@
 package desafios;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Desafio1 {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int b = Integer.parseInt(st.nextToken());
+        int total = a*b;
+        // Altere o valor da variável com o cálculo esperado
+        System.out.println("PROD = " + total);
+
     }
 
 }
