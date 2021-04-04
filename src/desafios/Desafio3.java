e31982c1dbaf608d1cfe6f5edccce329a01a0717
Marcelo Alves <marceloalves1295@gmail.com>
2021-04-04 15:54:18 -0300

    Terceiro desafio concluido

--- src/desafios/Desafio3.java
+++ src/desafios/Desafio3.java
@@ -1,4 +1,14 @@
 package desafios;
 
+import java.util.Scanner;
+
 public class Desafio3 {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int num = entrada.nextInt();
+        for (int i = 1; i <= num; i = i + 2) { //complete seu codigo aqui
+            System.out.println(i);
+        }
+    }
 }
